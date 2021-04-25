35ab7369d0c432747440259b32aa7585283d866c
JONNYJSC <jjonath21@gmail.com>
2021-04-25 11:04:58 -0600

    sento task

---
+++ js/firebase.js
@@ -0,0 +1,21 @@
+// let config = {
+//   apiKey: "AIzaSyDoJLhP2PwWM_Qt2kMXashZyd_2uIOl4eo",
+//   authDomain: "examplefirebase-9c4ca.firebaseapp.com",
+//   databaseURL: "https://examplefirebase-9c4ca.firebaseio.com",
+//   projectId: "examplefirebase-9c4ca",
+//   storageBucket: "",
+//   messagingSenderId: "739171130820",
+// };
+// firebase.initializeApp(config);
+
+let config = {
+  apiKey: "AIzaSyDFMLwa4rxcSAg9VjQOBxmmCWHn577w7nc",
+  authDomain: "webapp-ae02f.firebaseapp.com",
+  databaseURL: "https://webapp-ae02f.firebaseio.com",
+  projectId: "webapp-ae02f",
+  storageBucket: "webapp-ae02f.appspot.com",
+  messagingSenderId: "573592755089",
+  appId: "1:573592755089:web:722c9fc626b14f30e01830",
+  measurementId: "G-F6HQLTEHGQ"
+};
+firebase.initializeApp(config);
